04da87533416de5a6cace3ad6bd0bea952abfb04
KNDPuppi <sebastian.stapel@googlemail.com>
2023-07-14 14:24:46 +0200

    first commit

--- DBconn/Main.py
+++ DBconn/Main.py
@@ -6,14 +6,84 @@ import csv
 import mysql.connector
 import schedule
 import time
-from tkinter import Tk, Button, Entry, Label
+import ctypes
+import winreg
+import configparser
+from tkinter import Tk, ttk, Button, Entry, Label
 from tkinter.filedialog import asksaveasfilename
 from datetime import datetime, timedelta
 
 
+host = 'None'
+user = 'None'
+database ='None'
+
  #! ###########################################Aufruf Unterfunktionen Def###############################################################
 
-#? def- Schreibe alle Daten aus Datenbank in ein Excelfile 
+#? def- Aufruf aller Widgets und Design Funktionen 
+def HomeScreenConfig():
+
+    style = ttk.Style(fenster)
+
+    # Ermittle das aktuelle Fensterdesign von Windows
+    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize")
+    theme = winreg.QueryValueEx(key, "AppsUseLightTheme")[0]
+
+    # Passe das Thema an das Betriebssystem an
+    if theme == 0:
+        style.theme_use(themename="clam")
+    else:
+        style.theme_use(themename="dark")
+
+    fenster.title("DB Collector")
+    fenster.geometry("800x400")
+    
+    # Textfeld
+    label = tk.Label(fenster, text='01') 
+    label.pack(side="bottom"  )
+
+    #Button Save alles
+    button_DB_save_all = tk.Button(fenster, text="SaveAll!" , command=button_DBSaveAll)
+    button_DB_save_all .config(width=20, height=3)
+    button_DB_save_all .place(x=20, y=50, )
+
+  #Button Save aktuellen Tag 
+    button_DB_save_cd = tk.Button(fenster, text="SaveDay!" , command=lambda: button_DBSaveCurDay(entry))
+    button_DB_save_cd .config(width=20, height=3)
+    button_DB_save_cd .place(x=160, y=50, )
+    
+        
+    # Eingabefeld für das Datum hinzufügen
+    entry_label = Label(fenster, text="Datum (YYYY-MM-DD): ")
+    entry_label.pack()
+    entry = Entry(fenster)
+    entry.pack()
+
+     # Eingabefeld für das host
+    entry_host = Label(fenster, text="Host ")
+    entry_host.pack()
+    entry_host = Entry(fenster)
+    entry_host.pack()
+
+      # Eingabefeld für root
+    entry_root = Label(fenster, text="root ")
+    entry_root.pack()
+    entry_root = Entry(fenster)
+    entry_root.pack()
+
+      # Eingabefeld für Database
+    entry_db = Label(fenster, text="database ")
+    entry_db.pack()
+    entry_db = Entry(fenster)
+    entry_db.pack()
+
+          # Eingabefeld für root
+    entry_pw = Label(fenster, text="Password ")
+    entry_pw.pack()
+    entry_pw = Entry(fenster)
+    entry_pw.pack()
+   
+#? def- Schreibe alle Daten aus Datenbank in ein Excelfile (mit asksaveasfilename)  
 def button_DBSaveAll():
     db_connection = mysql.connector.connect(
     host="localhost",
@@ -45,15 +115,17 @@ def button_DBSaveAll():
     # Verbindung und Cursor schließen
     cursor.close()
     db_connection.close()
- 
-def button_DBSaveCurDay():
+
+#? def- Schreibe alle Daten eines Tages in ein Excelfile (mit asksaveasfilename)
+def button_DBSaveCurDay(inputDay):
+    
     db_connection = mysql.connector.connect(
     host="localhost",
     user="root",
     database="ford1419_st2_eol"
     )
     cursor = db_connection.cursor()
-    date = entry.get()
+    date= inputDay.get()
 
    
     select_query = "SELECT * FROM prozessdaten WHERE DATE(timestamp) = '" + date + "'"
@@ -81,38 +153,30 @@ def button_DBSaveCurDay():
     cursor.close()
     db_connection.close() 
 
+def Read_Config():
+    # Konfigurationsdatei erstellen oder laden
+    config = configparser.ConfigParser()
+    config.read('config.ini')
 
+    # Informationen aus der Konfigurationsdatei lesen
+    host = config.get('Database', 'Host')
+    user = config.get('Database', 'User')
+    database = config.get('Database', 'Database')
+  
 
-
-
-# Erstelle ein Fenster
+#! ########################################### GUI Aufruf und Hauptschleife###############################################################
+# Öffne Homescreen
 fenster = tk.Tk()
+#In jedem Zyklus wird die Config für das Hauptbil aufgerufen
+HomeScreenConfig()
+#Nach öffnen des fensters wird einmalig die Konfiguration für die Datenbank ausgelesen
+fenster.protocol("WM_DELETE_WINDOW", Read_Config())
 
-# Definiere den Titel des Fensters
-fenster.title("DB Collector")
 
-# Definiere die Größe des Fensters
-fenster.geometry("800x400")
 
-# Textfeld
-label = tk.Label(fenster, text='01') 
-label.pack(side="bottom"  )
 
-#Button Save alles
-button_DB_save_all = tk.Button(fenster, text="SaveAll!" , command=button_DBSaveAll)
-button_DB_save_all .config(width=20, height=3)
-button_DB_save_all .place(x=20, y=50, )
 
-#Button Save aktuellen Tag 
-button_DB_save_cd = tk.Button(fenster, text="SaveDay!" , command=button_DBSaveCurDay)
-button_DB_save_cd .config(width=20, height=3)
-button_DB_save_cd .place(x=160, y=50, )
 
-# Eingabefeld für das Datum hinzufügen
-entry_label = Label(fenster, text="Datum (YYYY-MM-DD): ")
-entry_label.pack()
-entry = Entry(fenster)
-entry.pack()
 
 
 # Starte die Hauptschleife des Fensters
