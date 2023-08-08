b63bad393c3221945c38cb8f0355ea4e80d4e331
KNDPuppi <sebastian.stapel@googlemail.com>
2023-08-08 10:18:08 +0200

    gf

---
+++ DBconn/Test/homescreen_config.py
@@ -0,0 +1,19 @@
+
+
+# homescreen_config.py
+
+import tkinter as tk
+from tkinter import Tk, ttk, Button, Entry, Label, Toplevel
+from datetime import datetime, timedelta
+
+def configure_home_screen(GUI):
+    # Hier kommen die Teile der Homescreen-Konfiguration
+    
+    entry_label = Label(GUI, text="Datum (YYYY-MM-DD): ")
+    entry_label.pack()
+
+
+# Weitere Funktionen oder Konstanten, die du benötigst
+
+
+   
