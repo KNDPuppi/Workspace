6d2008603d508323c8c7364f3e7772adb4276440
KNDPuppi <sebastian.stapel@googlemail.com>
2024-08-21 12:23:08 +0200

    Automatsiche backup

--- DBconn/Main.py
+++ DBconn/Main.py
@@ -6,12 +6,10 @@ import threading
 import time
 import datetime
 
-# Funktion zum Schreiben der Konfigurationsdatei
 def Button_write_DB_config(host, user, database):
     Write_Config(host, user, database, save_path_all_var.get(), save_path_day_var.get(), scheduled_time_var.get())
     messagebox.showinfo("Info", "Datenbankkonfiguration gespeichert!")
 
-# Funktion zum Speichern aller Daten
 def Button_Save_all_data():
     VarTransIn_Read_Config = Read_Config()
     host = VarTransIn_Read_Config['t_host']
@@ -19,7 +17,6 @@ def Button_Save_all_data():
     database = VarTransIn_Read_Config['t_database']
     DBSaveAll(host, user, database, save_path_all_var.get())
 
-# Funktion zum Speichern der Daten eines bestimmten Tages
 def Button_Save_CurrenDay(inputDay):
     VarTransIn_Read_Config = Read_Config()
     host = VarTransIn_Read_Config['t_host']
@@ -27,11 +24,11 @@ def Button_Save_CurrenDay(inputDay):
     database = VarTransIn_Read_Config['t_database']
     DBSaveCurDay(host, user, database, inputDay, save_path_day_var.get())
 
-# Funktion für die automatische Ausführung
 def scheduled_task():
     while True:
         now = datetime.datetime.now().time()
-        if now.strftime("%H:%M") == scheduled_time_str:
+        current_time_str = now.strftime("%H:%M")
+        if current_time_str == scheduled_time_str:
             VarTransIn_Read_Config = Read_Config()
             host = VarTransIn_Read_Config['t_host']
             user = VarTransIn_Read_Config['t_user']
@@ -39,12 +36,11 @@ def scheduled_task():
             inputDay = datetime.datetime.now().strftime("%Y-%m-%d")
             DBSaveCurDay(host, user, database, inputDay, save_path_day_var.get())
             time.sleep(60)  # Warten, um Mehrfachausführungen zu vermeiden
-        time.sleep(1)  # Jede Sekunde prüfen
+        else:
+            time.sleep(1)  # Jede Sekunde prüfen
 
-# Erstelle das Hauptfenster
 Main_GUI = tk.Tk()
 
-# Lese die Konfiguration
 VarTransIn_Read_Config = Read_Config()
 host = VarTransIn_Read_Config['t_host']
 user = VarTransIn_Read_Config['t_user']
@@ -53,12 +49,10 @@ save_path_all = VarTransIn_Read_Config['save_path_all']
 save_path_day = VarTransIn_Read_Config['save_path_day']
 scheduled_time_str = VarTransIn_Read_Config['scheduled_time']
 
-# Erstelle StringVars für die Pfade und die Zeit
 save_path_all_var = tk.StringVar(value=save_path_all)
 save_path_day_var = tk.StringVar(value=save_path_day)
 scheduled_time_var = tk.StringVar(value=scheduled_time_str)
 
-# Rufe die GUI-Konfiguration auf
 HomeScreenConfig(
     Main_GUI, 
     host, 
@@ -72,9 +66,7 @@ HomeScreenConfig(
     scheduled_time_var
 )
 
-# Starte den Scheduler in einem separaten Thread
 scheduler_thread = threading.Thread(target=scheduled_task, daemon=True)
 scheduler_thread.start()
 
-# Starte die Hauptschleife
 Main_GUI.mainloop()
