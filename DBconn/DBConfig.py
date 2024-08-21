49abe978d63ca7bdfb84fbe43160bd1c7dbbcf55
KNDPuppi <sebastian.stapel@googlemail.com>
2024-08-21 08:51:50 +0100

    test

--- DBconn/DBConfig.py
+++ DBconn/DBConfig.py
@@ -1,19 +1,21 @@
 
+import csv
+import mysql.connector
 import configparser
 import os
-import mysql.connector
-import csv
+
 from tkinter import Tk
-from tkinter.filedialog import asksaveasfilename, askdirectory
+from tkinter.filedialog import asksaveasfilename
 
 def Read_Config():
     script_directory = os.path.dirname(os.path.abspath(__file__))
     config_file_path = os.path.join(script_directory, 'config.ini')
 
     config = configparser.ConfigParser()
+
     if not os.path.isfile(config_file_path):
         print(f"Konfigurationsdatei nicht gefunden: {config_file_path}")
-        return {'t_host': '', 't_user': '', 't_database': '', 'save_path_all': '', 'save_path_day': ''}
+        return {'t_host': '', 't_user': '', 't_database': '', 'scheduled_time': ''}
 
     config.read(config_file_path)
 
@@ -21,23 +23,14 @@ def Read_Config():
         host = config.get('Database', 'Host')
         user = config.get('Database', 'User')
         database = config.get('Database', 'Database')
-        save_path_all = config.get('Paths', 'SavePathAll')
-        save_path_day = config.get('Paths', 'SavePathDay')
+        scheduled_time = config.get('Settings', 'ScheduledTime')
     except (configparser.NoSectionError, configparser.NoOptionError) as e:
         print(f"Fehler beim Lesen der Konfiguration: {e}")
-        return {'t_host': '', 't_user': '', 't_database': '', 'save_path_all': '', 'save_path_day': ''}
+        return {'t_host': '', 't_user': '', 't_database': '', 'scheduled_time': ''}
 
-    transfer_variables = {
-        't_host': host,
-        't_user': user,
-        't_database': database,
-        'save_path_all': save_path_all,
-        'save_path_day': save_path_day
-    }
+    return {'t_host': host, 't_user': user, 't_database': database, 'scheduled_time': scheduled_time}
 
-    return transfer_variables
-
-def Write_Config(host, user, database, save_path_all, save_path_day):
+def Write_Config(host, user, database, scheduled_time=None):
     script_directory = os.path.dirname(os.path.abspath(__file__))
     config_file_path = os.path.join(script_directory, 'config.ini')
 
@@ -50,15 +43,15 @@ def Write_Config(host, user, database, save_path_all, save_path_day):
         'Database': database
     }
 
-    config['Paths'] = {
-        'SavePathAll': save_path_all,
-        'SavePathDay': save_path_day
-    }
+    if scheduled_time:
+        if 'Settings' not in config:
+            config['Settings'] = {}
+        config['Settings']['ScheduledTime'] = scheduled_time
 
     with open(config_file_path, 'w') as config_file:
         config.write(config_file)
 
-def DBSaveAll(host, user, database, save_path_all):
+def DBSaveAll(host, user, database, file_path):
     try:
         db_connection = mysql.connector.connect(
             host=host,
@@ -66,19 +59,19 @@ def DBSaveAll(host, user, database, save_path_all):
             database=database
         )
         cursor = db_connection.cursor()
+
         select_query = "SELECT * FROM prozessdaten"
         cursor.execute(select_query)
         results = cursor.fetchall()
 
-        if not save_path_all:
+        if not file_path:
             root = Tk()
             root.withdraw()
-            save_path_all = asksaveasfilename(defaultextension=".csv", filetypes=[("CSV Dateien", "*.csv")])
-            if not save_path_all:
-                return  # Benutzer hat den Datei-Speicher-Dialog abgebrochen
-            root.destroy()
+            file_path = asksaveasfilename(defaultextension=".csv", filetypes=[("CSV Dateien", "*.csv")])
+            if not file_path:
+                return
 
-        with open(save_path_all, 'w', newline='') as csv_file:
+        with open(file_path, 'w', newline='') as csv_file:
             csv_writer = csv.writer(csv_file)
             column_names = [i[0] for i in cursor.description]
             csv_writer.writerow(column_names)
@@ -95,7 +88,7 @@ def DBSaveAll(host, user, database, save_path_all):
         except:
             pass
 
-def DBSaveCurDay(host, user, database, inputDay, save_path_day):
+def DBSaveCurDay(host, user, database, inputDay, file_path):
     try:
         db_connection = mysql.connector.connect(
             host=host,
@@ -104,19 +97,19 @@ def DBSaveCurDay(host, user, database, inputDay, save_path_day):
         )
         cursor = db_connection.cursor()
         date = inputDay
+
         select_query = "SELECT * FROM prozessdaten WHERE DATE(timestamp) = %s"
         cursor.execute(select_query, (date,))
         results = cursor.fetchall()
 
-        if not save_path_day:
+        if not file_path:
             root = Tk()
             root.withdraw()
-            save_path_day = asksaveasfilename(defaultextension=".csv", filetypes=[("CSV Dateien", "*.csv")])
-            if not save_path_day:
-                return  # Benutzer hat den Datei-Speicher-Dialog abgebrochen
-            root.destroy()
+            file_path = asksaveasfilename(defaultextension=".csv", filetypes=[("CSV Dateien", "*.csv")])
+            if not file_path:
+                return
 
-        with open(save_path_day, 'w', newline='') as csv_file:
+        with open(file_path, 'w', newline='') as csv_file:
             csv_writer = csv.writer(csv_file)
             column_names = [i[0] for i in cursor.description]
             csv_writer.writerow(column_names)
--- DBconn/GUIConfig.py
+++ DBconn/GUIConfig.py
@@ -1,15 +1,19 @@
 import tkinter as tk
 import winreg
-from tkinter import ttk, Label, Button, Toplevel
+from tkinter import ttk, Entry, Label, Button, Toplevel
 from tkinter.filedialog import askdirectory
 from tkcalendar import DateEntry
+from datetime import datetime, timedelta
+import threading
+import time
 
 def HomeScreenConfig(Main_GUI, host, user, database,
                      button_function_WrDB,
                      button_function_Save_all,
                      button_function_Save_Day,
                      save_path_all_var,
-                     save_path_day_var):
+                     save_path_day_var,
+                     time_entry_var):  # Neuer Parameter für die Uhrzeit
     try:
         style = ttk.Style(Main_GUI)
 
@@ -40,64 +44,50 @@ def HomeScreenConfig(Main_GUI, host, user, database,
         entry_Date.grid(row=0, column=1)
 
         # Eingabefelder
-        entry_host = ttk.Entry(user_frame_1, width=30, justify="right")
-        entry_host.grid(row=1, column=1)
-        entry_host.insert(0, host)
+        labels_and_entries = [
+            ("Host", entry_host := ttk.Entry(user_frame_1, width=23, justify="right")),
+            ("User", entry_user := ttk.Entry(user_frame_1, width=23, justify="right")),
+            ("Database", entry_db := ttk.Entry(user_frame_1, width=23, justify="right")),
+            ("Time", entry_time := ttk.Entry(user_frame_1, width=23, justify="right"))  # Neues Eingabefeld für die Zeit
+        ]
 
-        entry_user = ttk.Entry(user_frame_1, width=30, justify="right")
-        entry_user.grid(row=2, column=1)
-        entry_user.insert(0, user)
-
-        entry_db = ttk.Entry(user_frame_1, width=30, justify="right")
-        entry_db.grid(row=3, column=1)
-        entry_db.insert(0, database)
-
-        Label(user_frame_1, text="Host").grid(row=1, column=0)
-        Label(user_frame_1, text="User").grid(row=2, column=0)
-        Label(user_frame_1, text="Database").grid(row=3, column=0)
+        for i, (label_text, entry) in enumerate(labels_and_entries, start=1):
+            Label(user_frame_1, text=label_text).grid(row=i, column=0)
+            entry.grid(row=i, column=1)
+            entry.insert(0, locals()[f"entry_{label_text.lower()}"])
 
         # Zeige die gespeicherten Pfade an
         Label(user_frame_1, text="Save Path All").grid(row=8, column=0)
-        save_path_all_label = Label(user_frame_1, textvariable=save_path_all_var, fg="gray", wraplength=400)
+        save_path_all_label = Label(user_frame_1, textvariable=save_path_all_var, fg="gray")
         save_path_all_label.grid(row=8, column=1)
 
         Label(user_frame_1, text="Save Path Day").grid(row=9, column=0)
-        save_path_day_label = Label(user_frame_1, textvariable=save_path_day_var, fg="gray", wraplength=400)
+        save_path_day_label = Label(user_frame_1, textvariable=save_path_day_var, fg="gray")
         save_path_day_label.grid(row=9, column=1)
 
-        Button(user_frame_1, text="Select Save Path All", command=lambda: select_save_path_all(save_path_all_var)).grid(row=8, column=2)
-        Button(user_frame_1, text="Select Save Path Day", command=lambda: select_save_path_day(save_path_day_var)).grid(row=9, column=2)
+        Label(user_frame_1, text="Scheduled Time").grid(row=10, column=0)  # Neues Label für die Zeit
+        entry_time.grid(row=10, column=1)  # Neues Eingabefeld für die Zeit
 
         # Buttons
         button_width = 20  # Ändere die Breite nach Bedarf
         button_height = 4
 
         Button(user_frame_1, text="Save DB Config", width=button_width, height=button_height,
-               command=lambda: button_function_WrDB(entry_host.get(), entry_user.get(), entry_db.get())).grid(row=4, column=0, columnspan=3, sticky="e")
+               command=lambda: button_function_WrDB(entry_host.get(), entry_user.get(), entry_db.get(), entry_time.get())).grid(row=4, column=0, columnspan=2, sticky="e")
 
         Button(user_frame_1, text="Save DB ALL", width=button_width, height=button_height,
-               command=button_function_Save_all).grid(row=5, column=0, columnspan=3, sticky="e")
+               command=lambda: button_function_Save_all()).grid(row=5, column=0, columnspan=2, sticky="e")
 
         Button(user_frame_1, text="Save DB Day", width=button_width, height=button_height,
-               command=lambda: button_function_Save_Day(entry_Date.get())).grid(row=6, column=0, columnspan=3, sticky="e")
+               command=lambda: button_function_Save_Day(entry_Date.get())).grid(row=6, column=0, columnspan=2, sticky="e")
 
         Button(user_frame_1, text="DB Config", width=button_width, height=button_height,
-               command=lambda: open_database_settings(Main_GUI)).grid(row=7, column=0, columnspan=3, sticky="e")
+               command=lambda: open_database_settings(Main_GUI)).grid(row=7, column=0, columnspan=2, sticky="e")
 
         for widget in user_frame_1.winfo_children():
             widget.grid_configure(padx=20, pady=5)
     except Exception as e:
         print(f"Fehler beim Erstellen der GUI: {e}")
-
-def select_save_path_all(save_path_var):
-    path = askdirectory(title="Select Directory for Saving All Data")
-    if path:
-        save_path_var.set(path)
-
-def select_save_path_day(save_path_var):
-    path = askdirectory(title="Select Directory for Saving Daily Data")
-    if path:
-        save_path_var.set(path)
 
 def open_database_settings(Main_GUI):
     db_settings_window = Toplevel(Main_GUI)
